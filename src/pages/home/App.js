98f79b57c315f233de3b9c82cef60f82b808c005
Sebastian Ruiz <imsebarz@gmail.com>
2022-04-12 11:38:27 -0500

    Initial layout

---
+++ src/pages/home/App.js
@@ -0,0 +1,41 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <h1>NAVBAR</h1>
+        </nav>
+      </header>
+      <main>
+        <section className="section-hero">
+          <h1>section-hero</h1>
+        </section>
+        <section className="section-register">
+          <h1>section-register</h1>
+        </section>
+        <section className="section-arena">
+          <h1>section-arena</h1>
+        </section>
+        <section className="section-copy">
+          <h1>section-copy</h1>
+        </section>
+        <section className="section-relics">
+          <h1>section-relics</h1>
+        </section>
+        <section className="section-evolutions">
+          <h1>section-evolutions</h1>
+        </section>
+        <section className="section-cta">
+          <h1>section-cta</h1>
+        </section>
+        <footer>
+          <h1>footer</h1>
+        </footer>
+      </main>
+    </div>
+  );
+}
+
+export default App;
