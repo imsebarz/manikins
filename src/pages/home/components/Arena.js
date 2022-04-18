494a3fcdac34a6908a23beb87061a30a999db6fe
Sebastian Ruiz <imsebarz@gmail.com>
2022-04-18 18:14:33 -0500

    added alt

--- src/pages/home/components/Arena.js
+++ src/pages/home/components/Arena.js
@@ -5,7 +5,7 @@ import bluemaniki from '../../../assets/bluemaniki.png'
 const Arena = () => {
     return (
         <section className="section-arena">
-            <img src={bluemaniki} />
+            <img src={bluemaniki} alt='Manikin' />
             <div className='content'>
                 <h1>Enter the arena</h1>
                 <p>Manikins  is a Tactic game and NFT Relics collector built on the Immutable Blockchain, releasing on PC  in 2022.</p>
