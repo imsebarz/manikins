3238e7521e9e66a575e6b2bb05b7357b7d83acfd
Sebastian Ruiz <imsebarz@gmail.com>
2022-04-19 08:51:43 -0500

    evolutions section

--- src/pages/home/components/Evolutions.js
+++ src/pages/home/components/Evolutions.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import evolutions from '../../../assets/evolutions.png'
+import './evolutions.scss'
 
 const Evolutions = () => {
     return (
         <section className="section-evolutions">
-            <div>
+            <div className='content'>
                 <h1>Every Relic has evolutions</h1>
                 <p>With every evolution the relic unlocks new features and powers</p>
+            </div>
+            <img src={evolutions} alt='magic book with evolutions' />
+            <div className='characteristics'>
+                <div>
+                    <p>🟢</p>
+                    <p>Tradeable</p>
+                </div>
+                <div>
+                    <p>🟢</p>
+                    <p>Earn</p>
+                </div>
+                <div>
+                    <p>🟢</p>
+                    <p>Limited</p>
+                </div>
             </div>
         </section>
     )
