461cc1422c63a96255e80a2b2f0c7a6f6b23666b
Sebastian Ruiz <imsebarz@gmail.com>
2022-04-25 09:02:15 -0500

    updated to react 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './pages/home/App';
+import Login from './pages/login/Login';
 // import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from './pages/register/Register';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={< Login />} />
+        <Route path="/register" element={< Register />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
