9dbba2a644ab2b1053e4b9b55cf06c5b7dc53677
Sebastian Ruiz <imsebarz@gmail.com>
2022-04-27 09:02:12 -0500

    bugfix

--- src/index.js
+++ src/index.js
@@ -6,19 +6,22 @@ import Login from './pages/login/Login';
 // import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from './pages/register/Register';
+import { UserProvider } from './context/user/State';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={< Login />} />
-        <Route path="/register" element={< Register />} />
-      </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={< Login />} />
+          <Route path="/register" element={< Register />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   </React.StrictMode>
 );
 
