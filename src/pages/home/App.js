7bef9c5aeae603517d5593a9763204d958ff2151
Sebastian Ruiz <imsebarz@gmail.com>
2022-05-18 10:00:10 -0500

    Added Team and Contact Sections

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,18 +1,20 @@
-import Footer from "../../Components/Footer";
-import Navbar from "../../Components/Navbar";
-import "./App.scss";
-import About from "./components/About";
-import Cta from "./components/Cta.js";
-import Hero from "./components/Hero";
-import Coffers from "./components/Coffers";
-import Evolutions from "./components/Evolutions";
-import ComingSoon from "./components/ComingSoon";
-import Carousel from "../../Components/Carousel";
-import Costumes from "./components/Costumes";
-import Features from "./components/Features";
-import Nfts from "./components/Nfts";
+import Footer from '../../Components/Footer'
+import Navbar from '../../Components/Navbar'
+import './App.scss'
+import About from './components/About'
+import Cta from './components/Cta.js'
+import Hero from './components/Hero'
+import Coffers from './components/Coffers'
+import Evolutions from './components/Evolutions'
+import ComingSoon from './components/ComingSoon'
+import Carousel from '../../Components/Carousel'
+import Costumes from './components/Costumes'
+import Features from './components/Features'
+import Nfts from './components/Nfts'
+import Team from './components/Team'
+import Contact from './components/Contact'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
@@ -28,11 +30,13 @@ function App() {
         <Carousel></Carousel>
         <Costumes></Costumes>
         <Features></Features>
+        <Team></Team>
+        <Contact></Contact>
         <Cta></Cta>
         <Footer></Footer>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/home/components/Team.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <section>
+      <div className="title">
+        <h1 className="golden">Our Team</h1>
+        <p>
+          We are a specialized and passionate group in the design and
+          development of video games, with the aim of providing the best
+          experiences to our players by implementing the best technologies.
+        </p>
+      </div>
+      <div className="slider"></div>
+    </section>
+  )
+}
+
+export default Team
