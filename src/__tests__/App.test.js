af1aef497c4b142e470698cba57b0a1f5f4bfcc9
Sebastian Ruiz <imsebarz@gmail.com>
2022-06-12 20:13:17 -0500

    updated test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react'
-import App from '../pages/home/App'
+// import { render, screen } from '@testing-library/react'
+// import App from '../pages/home/App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+test('Here you put some tests', () => {
+
 })
