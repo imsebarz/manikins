1fe832baa175230f8564b56bafe87907cb4976af
Sebastian Ruiz <imsebarz@gmail.com>
2022-06-24 10:36:43 -0500

    updated separator and background

--- src/pages/home/components/ComingSoon/ComingSoon.js
+++ src/pages/home/components/ComingSoon/ComingSoon.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import './comingSoon.scss'
 import floatingItems from '../../../../assets/floatingItems.png'
+import separatorTop from '../../../../assets/separatorTop.svg'
 
 const ComingSoon = () => {
   return (
-    <section className="section-comingsoon">
-      <div className="content">
-        <h3>NFT's presale</h3>
-        <h1 className="golden">COMING SOON</h1>
-        <h3>
-          140<span>days </span> 50<span>hrs </span> 30<span>mins </span>
-          00<span>secs</span>
-        </h3>
-        <p>Be part of our new community!</p>
-        <button>Join Discord!</button>
-        <img src={floatingItems} className='floating-items' alt="Floating items" />
-      </div>
-    </section>
+    <>
+      <img src={separatorTop} className='separator' alt="" />
+      <section className="section-comingsoon">
+        <div className="content">
+          <h3>NFT's presale</h3>
+          <h1 className="golden">COMING SOON</h1>
+          <h3>
+            140<span>days </span> 50<span>hrs </span> 30<span>mins </span>
+            00<span>secs</span>
+          </h3>
+          <p>Be part of our new community!</p>
+          <button>Join Discord!</button>
+          <img src={floatingItems} className='floating-items' alt="Floating items" />
+        </div>
+      </section>
+    </>
   )
 }
 
