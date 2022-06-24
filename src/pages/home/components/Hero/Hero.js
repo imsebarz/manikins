2939153764b7c33360954302ce8128c64095ddf0
Sebastian Ruiz <imsebarz@gmail.com>
2022-06-24 10:36:59 -0500

    redesigned hero section

--- src/pages/home/components/Hero/Hero.js
+++ src/pages/home/components/Hero/Hero.js
@@ -1,27 +1,16 @@
 import React from 'react'
-import particles from '../../../../assets/particles.mp4'
 import hero from '../../../../assets/hero.png'
 import tipografia from '../../../../assets/tipografia.png'
-import arrow from '../../../../assets/arrow.png'
+import arrow from '../../../../assets/arrow.svg'
 import './hero.scss'
 
 const Hero = () => {
   return (
     <section className="section-hero">
-      <video
-        className="video-back"
-        autoPlay
-        loop
-        muted
-        onClick={(e) => (e.target.muted = !e.target.muted)}
-      >
-        <source src={particles} type="video/ogg" />
-      </video>
       <img className="hero-image" src={hero} alt="hero" />
+      <div className="gradient"></div>
       <div className="content">
-        <img src={tipografia} alt="hero" />
-        <h3>Buy, improve and sell NFTs while you play with our new game.</h3>
-        <button className="goldenButton">{"View NFT's"}</button>
+        <img src={tipografia} className='title' alt="hero" />
         <img src={arrow} className="arrow" alt="hero" />
       </div>
     </section>
