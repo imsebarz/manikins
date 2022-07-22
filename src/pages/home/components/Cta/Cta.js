1cefb6160f50a24d04a377042739f0fec4fda5b3
Sebastian Ruiz <imsebarz@gmail.com>
2022-07-22 10:50:21 -0500

    deleted contact section

--- src/pages/home/components/Cta/Cta.js
+++ src/pages/home/components/Cta/Cta.js
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './cta.scss'
 import { joinWaitList } from '../../../../context/user/Actions'
 import { useUserDispatch } from '../../../../context/user/State'
+import textseparator from '../../../../assets/textseparator.svg'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const Cta = () => {
   const [mail, setMail] = useState('')
@@ -15,14 +17,15 @@ const Cta = () => {
 
   return (
     <section className="section-cta">
-      <h1 className='golden'>JOIN THE WAITLIST</h1>
+      <h1 className='golden'>REGISTER NOW!</h1>
+      <LazyLoadImage threshold={800} src={textseparator} className="text-separator" alt="hero" />
       <div className='cta-input'>
         <p>
           Register now and be the first to know about manikins beta, relics sales, events and more!
         </p>
         <form className='cta-input-container' onSubmit={(e) => handleSubmit(e)}>
           <input type='email' required placeholder='Enter your email' value={mail} onChange={(e) => setMail(e.target.value)}></input>
-          <button className='goldenButton'>Join now!</button>
+          <button className='goldenButton'>REGISTER</button>
         </form>
       </div>
     </section>
