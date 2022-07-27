a7edaf9ea97c1ae5b61abbb47bdf30e8f998986d
Sebastian Ruiz <imsebarz@gmail.com>
2022-07-27 10:35:02 -0500

    Resdesigned footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,30 +1,56 @@
 import React from 'react'
 import './footer.scss'
-import logo from '../assets/logo.png'
-// import litgLogo from "../assets/litgLogo.png";
-// import fryosLogo from "../assets/fryosLogo.png";
-import instagram from '../assets/instagram.png'
-import facebook from '../assets/facebook.png'
-import youtube from '../assets/youtube.png'
-import discord from '../assets/discord.png'
+import logo from '../assets/manikins.png'
+import fryos from "../assets/fryosFooter.png";
+import litg from "../assets/litgFooter.png";
+import instagram from '../assets/instagram.svg'
+import facebook from '../assets/facebook.svg'
+import twitter from '../assets/twitter.svg'
+import youtube from '../assets/youtube.svg'
+import discord from '../assets/discord.svg'
+import hiManikin from "../assets/hiManikin.png";
+
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const Footer = () => {
   return (
     <footer>
       <div className="links">
+        <div className="logo-container">
         <LazyLoadImage threshold={800} src={logo} alt="" className="logo" />
+        </div>
         <div className="social">
+          <a href="https://www.facebook.com/" target='_blank' rel="noreferrer">
           <LazyLoadImage threshold={800} src={facebook} alt="" />
+          </a>
+          <a href="https://www.youtube.com/" target='_blank' rel="noreferrer">
           <LazyLoadImage threshold={800} src={youtube} alt="" />
+          </a>
+          <a href="https://www.instagram.com/" target='_blank' rel="noreferrer">
           <LazyLoadImage threshold={800} src={instagram} alt="" />
+          </a>
+          <a href="https://www.twitter.com/" target='_blank' rel="noreferrer">
+          <LazyLoadImage threshold={800} src={twitter} alt="" />
+          </a>
+          <a href="https://www.discord.com/" target='_blank' rel="noreferrer">
           <LazyLoadImage threshold={800} src={discord} alt="" />
+          </a>
+        </div>
+        <div className="companies">
+          <LazyLoadImage threshold={800} src={fryos} alt="" />
+          <LazyLoadImage threshold={800} src={litg} alt="" />
         </div>
       </div>
       <div className="copy">
         <p>Game development made with ❤️ from Colombia</p>
-        <p>Manikins Arena &copy; 2022. All rigths reserved.</p>
+        <small>Manikins Arena &copy; 2022. All rigths reserved.</small>
       </div>
+      <LazyLoadImage
+        threshold={800}
+        src={hiManikin}
+        className="hi-manikin"
+        alt="Hi gesture manikin"
+      />
     </footer>
   )
 }
