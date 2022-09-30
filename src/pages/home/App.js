43daba8364c3348b64f95c2cf42c22f85f244126
Sebastian Ruiz <imsebarz@gmail.com>
2022-09-30 10:09:17 -0500

    hide sections

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -4,12 +4,12 @@ import './App.scss'
 import About from './components/About/About'
 import Cta from './components/Cta/Cta'
 import Hero from './components/Hero/Hero'
-import Coffers from './components/Coffers/Coffers'
+// import Coffers from './components/Coffers/Coffers'
 import Evolutions from './components/Evolutions/Evolutions'
-import ComingSoon from './components/ComingSoon/ComingSoon'
+// import ComingSoon from './components/ComingSoon/ComingSoon'
 import Nfts from './components/Nfts/Nfts'
 import Team from './components/Team/Team'
-import Features from './components/Features/Features'
+// import Features from './components/Features/Features'
 import Roadmap from './components/Roadmap/Roadmap'
 import { useScroll } from '../../custom-hooks/useScroll'
 import { useEffect, useState } from 'react'
@@ -42,12 +42,12 @@ function App() {
       <Navbar></Navbar>
       <main>
         <Hero></Hero>
-        <ComingSoon></ComingSoon>
+        {/* <ComingSoon></ComingSoon> */}
         <About></About>
         <Nfts></Nfts>
         <Evolutions></Evolutions>
-        <Coffers></Coffers>
-        <Features></Features>
+        {/* <Coffers></Coffers> */}
+        {/* <Features></Features> */}
         <Roadmap></Roadmap>
         <Team></Team>
         <Cta></Cta>
